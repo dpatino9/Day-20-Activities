1a31429efcc6cc51aadb4032f9523dee30edd638
Diego Patiño <dapny125@gmail.com>
2016-10-25 17:46:42 -0400

    addedclasswork

---
+++ BuyLowSellHigh/buyLowsellHigh.js
@@ -0,0 +1,27 @@
+/**
+ * Buy Low, Sell High -- Starter Code
+ **/
+
+// Stock Prices
+iagStockPrices = [1.32, 1.14, 1.45, 1.20, 1.34, 1.74, 1.18, 1.90, 1.1];
+
+
+// Your Biggest Profit function
+var biggestProfit = function(stockArray, sharesBought){
+
+var stockPurchased = stockArray[2] * 1000 == "totalShares"
+console.log(stockArray)
+
+for (var i = 0; i < iagStockPrices.length; i++) {
+};
+
+// A Call to your Biggest Profit function.
+biggestProfit(iagStockPrices, 10000); 
+
+
+function 
+// NOTE: This should return 7600, 
+// because you could have bought it at 1.14 per share 
+// and then sold it at 1.90 per share. 
+// 1.90 - 1.14 = 0.76. 0.76 * 10000 is 7600.
+
